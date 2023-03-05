9a41a55f426c167e271ecb74568b84d82c9161d7
KIM JINJAE <jinjae.dev@gmail.com>
2023-03-05 16:34:32 +0900

    add: project init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './04-effects/App';
+// import Cleanup from './04-effects/Cleanup';
+// import ToDoList from './05-small-projects/ToDoList';
+// import Coin from './05-small-projects/Coin';
+// import CoinConverter from './05-small-projects/CoinConverter';
+import Movie from './06-movie-website-react/components/Movie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    {/* <App /> */}
+    {/* <Cleanup /> */}
+    {/* <ToDoList /> */}
+    {/* <Coin /> */}
+    {/* <CoinConverter /> */}
+    <Movie />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
